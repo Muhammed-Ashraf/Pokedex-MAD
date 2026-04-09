4036feb7f5a5ee22ba1dc5ba1c6b4b351fd046a7
ashraf probook <ashraf8534@gmail.com>
2026-04-09 11:28:03 +0400

    Add libraries for preview to render correctly

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -20,7 +20,14 @@ dependencies {
     api(libs.androidx.core.splashscreen)
 
     api(platform(libs.androidx.compose.bom))
+
+
+    api(libs.androidx.compose.runtime)
     api(libs.androidx.compose.ui)
-    api(libs.androidx.compose.material3)
+    api(libs.androidx.compose.ui.tooling)
     api(libs.androidx.compose.ui.tooling.preview)
+    api(libs.androidx.compose.animation)
+    api(libs.androidx.compose.material3)
+    api(libs.androidx.compose.foundation)
+    api(libs.androidx.compose.foundation.layout)
 }
