a46109e2c0594f5e0bb0242ca651639e2ce4d814
ashraf probook <ashraf8534@gmail.com>
2026-04-22 15:20:23 +0400

    complete Settings ViewModel testing

--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -6,3 +6,9 @@ plugins {
 android {
     namespace = "ashraf.pokedex.mad.feature.settings"
 }
+
+dependencies {
+    testImplementation(projects.core.test)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.junit)
+}
