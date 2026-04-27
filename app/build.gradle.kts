af15957c1ed02fb372ded22a5f108db53e3355a4
ashraf probook <ashraf8534@gmail.com>
2026-04-27 11:41:04 +0400

    update .gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 
     buildTypes { //todo
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
